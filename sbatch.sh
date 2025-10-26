dc299ecadad66657e4436c8d34a5e05cedf60171
Afra <connect@afra.in>
2025-10-25 21:38:41 -0400

    more files

---
+++ sbatch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=llama-finetune
+#SBATCH --nodes=3
+#SBATCH -D .
+#SBATCH --output=O-%x_%j.txt
+#SBATCH --error=E-%x_%j.txt
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-task=120
+#SBATCH --export=ALL
+
+export OMP_NUM_THREADS=120
+cd $SLURM_SUBMIT_DIR
+srun srun.sh
