66d009c89c3287f74bced51c2bdc7ef2e28a58fe
afra-ansaria <43047770+afra-ansaria@users.noreply.github.com>
2025-10-29 18:13:47 -0400

    Update infersrun.sh

--- infersrun.sh
+++ infersrun.sh
@@ -1,4 +1,3 @@
-
 # srun --gres=gpu:4 --cpus-per-task=4 python run_infer.py
 #srun --gres=gpu:4 --cpus-per-task=4 --mem=32G   uvicorn serve_infer:app --host 0.0.0.0 --port 8000
 #For curl run the inferAPI.py
