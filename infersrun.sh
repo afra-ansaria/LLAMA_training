aa975e5e3a850564d2a3b1cbb8542cfc19dbdf57
afra-ansaria <43047770+afra-ansaria@users.noreply.github.com>
2025-10-29 18:13:38 -0400

    Update infersrun.sh

--- infersrun.sh
+++ infersrun.sh
@@ -1,1 +1,6 @@
+
 # srun --gres=gpu:4 --cpus-per-task=4 python run_infer.py
+#srun --gres=gpu:4 --cpus-per-task=4 --mem=32G   uvicorn serve_infer:app --host 0.0.0.0 --port 8000
+#For curl run the inferAPI.py
+
+# curl -X POST http://10.6.60.253:8000/generate   -H "Content-Type: application/json"   -d '{"prompt":"Explain quantum computing to a 15-year-old. Keep your answers to 150 words","max_tokens":200}'
