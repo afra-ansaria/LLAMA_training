56ed30344802e673e1a7d175bb84e8e82e77cd35
Afra <connect@afra.in>
2025-10-30 15:50:24 -0400

    import os

--- task2/inferenceWithExample.py
+++ task2/inferenceWithExample.py
@@ -1,5 +1,6 @@
 # run_infer.py. Simple example file. You can change the prompt below. 
 import torch
+import os
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../models"))
